9494ae7daffb06e35731ad1080b0f20cdfee1e76
Mantycore <mantycore@gmail.com>
2019-04-08 19:10:09 +0300

    changed currency backend to exchangeratesapi; added auto calculation on start; tweaked the style a bit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -22,48 +22,66 @@ export class AppComponent {
     try {
       this.selectedCart = JSON.parse(value);
       this.error = null;
-      this.totalCartPriceJSON = "[ waiting for currency data ]";
       this.process();
     } catch(e) {
       this.error = "Input is not a valid JSON";
     }
   }
 
+  ngOnInit() {
+    this.process();
+  }
+
   process() {
     function pluck(arr, prop) {
-        return arr.map(el => el[prop]);
+      return arr.map(el => el[prop]);
     }
 
     function sum(arr) {
-        return arr.reduce((acc, cur) => acc + cur, 0);
+      return arr.reduce((acc, cur) => acc + cur, 0);
     }
 
     function reduceToObject(arr, fun) {
-        return arr.reduce((acc, cur) => {
-            acc[cur] = fun(cur);
-            return acc;
-        }, {});
+      return arr.reduce((acc, cur) => {
+        acc[cur] = fun(cur);
+        return acc;
+      }, {});
     }
 
+    /*const symbolsToPairs = {
+      rubles: 'USD_RUB',
+      euros: 'USD_EUR',
+      pounds: 'USD_GBP',
+      yens: 'USD_JPY'
+    };*/
     const symbolsToPairs = {
-        rubles: 'USD_RUB',
-        euros: 'USD_EUR',
-        pounds: 'USD_GBP',
-        yens: 'USD_JPY'
+      rubles: 'RUB',
+      euros: 'EUR',
+      pounds: 'GBP',
+      yens: 'JPY'
     };
 
     const sumUSD = sum(pluck(this.selectedCart, 'price'));
 
-    fetch("https://free.currencyconverterapi.com/api/v6/convert?q=USD_RUB,USD_EUR,USD_GBP,USD_JPY&compact=ultra&apiKey=c0ddd85505d82b4f8d06"
-    )
-        .then(res => res.json())
-        .then(pairs => Object.assign({
-                "US dollars": sumUSD
-            }, reduceToObject(
-                Object.keys(symbolsToPairs),
-                cur => pairs[symbolsToPairs[cur]] * sumUSD
-            )
-        ))
-        .then(result => { this.totalCartPriceJSON = JSON.stringify(result); });
+    this.totalCartPriceJSON = "[ waiting for currency data ]";
+
+    const exchangeURL = "https://api.exchangeratesapi.io/latest?base=USD&symbols=RUB,EUR,GBP,JPY";
+    //exchangeURL = "https://free.currencyconverterapi.com/api/v6/convert?q=USD_RUB,USD_EUR,USD_GBP,USD_JPY&compact=ultra&apiKey=c0ddd85505d82b4f8d06";
+
+    fetch(exchangeURL, {mode: 'cors'})
+      .then(res => res.json())
+      .then(json => json.rates) //unneeded for currencyconverterapi
+      .then(pairs => Object.assign({
+          "US dollars": sumUSD
+        }, reduceToObject(
+          Object.keys(symbolsToPairs),
+          cur => pairs[symbolsToPairs[cur]] * sumUSD
+        )
+      ))
+      .then(result => { this.totalCartPriceJSON = JSON.stringify(result); })
+      .catch(e => {
+        this.error = "Error during a request for currency data";
+        this.totalCartPriceJSON = "";
+      });
   }
 }
